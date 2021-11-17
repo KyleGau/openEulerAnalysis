3d6536e05eca71b1ce2541fae7f0518c6b690340
Kai Gao <gaokaipku@qq.com>
2021-11-17 15:04:00 +0800

    store commit and file changes to mongodb

--- README.md
+++ README.md
@@ -14,3 +14,13 @@ python getOrgRepos.py src-openeuler
 ```
 ./gitClone.sh
 ```
+3. Get commit logs and deltas
+
+```shell
+./commitLog.sh
+gunzip -c data/linux.log.gz | perl bin/extrgitPlog.perl | gzip > data/linux.delta.gz
+```
+4. Save commit content and file change history to MongoDB
+```shell
+python get_commit_history.py
+```
---
+++ get_commit_history.py
@@ -0,0 +1,68 @@
+import re
+from tqdm import tqdm
+from datetime import datetime
+from pymongo import MongoClient
+
+log_path = '/data/gitee_repos/openeuler.log'
+# log_path = 'sample'
+log_file = open(log_path, errors='ignore')
+content = log_file.read()
+log_file.close()
+
+client = MongoClient(host='127.0.0.1', port=27017)
+db = client['openeuler']
+commits_collection = db['commits']
+commits_collection.drop()
+commits_collection = db['commits']
+file_history_collection = db['file_history']
+file_history_collection.drop()
+file_history_collection = db['file_history']
+
+records = list(content.split('STARTOFTHECOMMIT:')[1:])
+docs = []
+file_history = []
+for record in tqdm(records):
+    try:
+        info, modifies = record.rsplit('NOTES\n', 1)
+        repo, ctp, body = info.split('\n', 2)
+        commit, tree, parent_commits, author_name, author_email, author_time, committer_name, committer_email, committer_time, title = ctp.split(';', 9)
+        parent_commits = parent_commits.split(' ')
+        doc = {
+            "repository": repo,
+            "commit": commit,
+            "tree": tree,
+            "parant_commits": parent_commits,
+            "author_name": author_name,
+            "author_email": author_email,
+            "author_time": datetime.utcfromtimestamp(int(author_time)),
+            "committer_name": committer_name,
+            "committer_email": committer_email,
+            "committer_time": datetime.utcfromtimestamp(int(committer_time)),
+            "message": title + '\n' + body
+        }
+        modifies = modifies.split('\n')[1:-1]
+        if modifies != ['']: 
+            pattern = re.compile('(?P<addition>(\d+|-))\s*(?P<deletion>(\d+|-))\s*(?P<file_name>.*)')
+            for item in modifies:
+                tmp = pattern.search(item).groupdict()
+                addtion, deletion, file_name = tmp['addition'], tmp['deletion'], tmp['file_name']
+                if addtion == '-':
+                    addtion = 0
+                if deletion == '-':
+                    deletion = 0
+                file_change = {
+                    "repository": repo,
+                    "file_name": file_name,
+                    "commit": commit,
+                    "addition": int(addtion),
+                    "deletion": int(deletion)
+                }
+                file_history.append(file_change)
+    except:
+        print(record)
+        print(item)
+        break
+    docs.append(doc)
+print("commits: {}, file changes: {}".format(len(docs), len(file_history)))
+commits_collection.insert_many(docs)
+file_history_collection.insert_many(file_history)

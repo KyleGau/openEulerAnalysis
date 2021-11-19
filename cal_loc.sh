09f64c65e0d36fa39f73a76301076adc6f2447c1
Kai Gao <gaokaipku@qq.com>
2021-11-19 21:20:35 +0800

    refactor code to be more usable

--- cal_loc.sh
+++ cal_loc.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 wd=`pwd`
 folder='/data/gitee_repos/openeuler/'
+year=$1
+month=$2
+day=$3
+echo $year $month $day
 
-cut -d\, -f1,2 --output-delimiter=" " $wd/data/2010-12-31.csv | while read repo commit; do
+if [ -f $wd/data/$year-$month-$day.loc ]; then rm $wd/data/$year-$month-$day.loc; fi
+cut -d\, -f1,2 --output-delimiter=" " $wd/data/$year-$month-$day.csv | while read repo commit; do
     echo $repo $commit
     cd $folder$repo
     git checkout --quiet -
@@ -10,18 +15,18 @@ cut -d\, -f1,2 --output-delimiter=" " $wd/data/2010-12-31.csv | while read repo 
     loc=`cloc --quiet . | grep '^SUM:' | tr -s ' ' | cut -d ' ' -f5`
     git checkout --quiet -
     echo $repo,$commit,$loc
-    echo $repo,$commit,$loc>>$wd/data/2010-12-31.loc
+    echo $repo,$commit,$loc>>$wd/data/$year-$month-$day.loc
 done
-paste -d , $wd/data/2010-12-31.csv <(cut -d\, -f3 $wd/data/2010-12-31.loc) > $wd/data/2010-12-31
+paste -d , $wd/data/$year-$month-$day.csv <(cut -d\, -f3 $wd/data/$year-$month-$day.loc) > $wd/data/$year-$month-$day
 
-cut -d\, -f1,2 --output-delimiter=" " $wd/data/2021-10-31.csv | while read repo commit; do
-    echo $repo $commit
-    cd $folder$repo
-    git checkout --quiet -
-    git checkout --quiet $commit
-    loc=`cloc --quiet . | grep '^SUM:' | tr -s ' ' | cut -d ' ' -f5`
-    git checkout --quiet -
-    echo $repo,$commit,$loc
-    echo $repo,$commit,$loc>>$wd/data/2021-10-31.loc
-done
-paste -d , $wd/data/2021-10-31.csv <(cut -d\, -f3 $wd/data/2021-10-31.loc) > $wd/data/2021-10-31
+# cut -d\, -f1,2 --output-delimiter=" " $wd/data/2021-10-31.csv | while read repo commit; do
+#     echo $repo $commit
+#     cd $folder$repo
+#     git checkout --quiet -
+#     git checkout --quiet $commit
+#     loc=`cloc --quiet . | grep '^SUM:' | tr -s ' ' | cut -d ' ' -f5`
+#     git checkout --quiet -
+#     echo $repo,$commit,$loc
+#     echo $repo,$commit,$loc>>$wd/data/2021-10-31.loc
+# done
+# paste -d , $wd/data/2021-10-31.csv <(cut -d\, -f3 $wd/data/2021-10-31.loc) > $wd/data/2021-10-31
--- retrieve_snapshot.py
+++ retrieve_snapshot.py
@@ -1,82 +1,52 @@
-from datetime import datetime, tzinfo, timezone
+from datetime import timezone
 from pymongo import MongoClient
+import sys
+import datetime
 
-client = MongoClient(host='127.0.0.1', port=27017)
-result = client['openeuler']['commits'].aggregate([
-    {
-        '$project': {
-            '_id': 0, 
-            'repository': 1, 
-            'commit': 1, 
-            'author_time': 1
-        }
-    }, {
-        '$match': {
-            'author_time': {
-                '$gte': datetime(2010, 12, 31, 0, 0, 0, tzinfo=timezone.utc), 
-                '$lt': datetime(2011, 1, 1, 0, 0, 0, tzinfo=timezone.utc)
+year = int(sys.argv[1])
+month = int(sys.argv[2])
+day = int(sys.argv[3])
+
+def retrieve_latest_commits(year, month, day):
+    client = MongoClient(host='127.0.0.1', port=27017)
+    result = client['openeuler']['commits'].aggregate([
+        {
+            '$project': {
+                '_id': 0, 
+                'repository': 1, 
+                'commit': 1, 
+                'author_time': 1
+            }
+        }, {
+            '$match': {
+                'author_time': { 
+                    '$lt': datetime.datetime(year, month, day, 0, 0, 0, tzinfo=timezone.utc) + datetime.timedelta(days=1)
+                }
+            }
+        }, {
+            '$sort': {
+                'repository': 1, 
+                'author_time': 1
+            }
+        }, {
+            '$group': {
+                '_id': '$repository', 
+                'commit': {
+                    '$last': '$commit'
+                }, 
+                'author_time': {
+                    '$last': '$author_time'
+                }
             }
         }
-    }, {
-        '$sort': {
-            'repository': 1, 
-            'author_time': 1
-        }
-    }, {
-        '$group': {
-            '_id': '$repository', 
-            'commit': {
-                '$last': '$commit'
-            }, 
-            'author_time': {
-                '$last': '$author_time'
-            }
-        }
-    }
-], allowDiskUse=True)
+    ], allowDiskUse=True)
+    result = list(result)
+    print(len(result))
+    # for doc in result:
+    #     print(doc['_id'], doc['commit'], str(doc['author_time']))
+    with open('data/{}-{}-{}.csv'.format(year, month, day), 'w') as outf:
+        for doc in result:
+            outf.write(','.join([doc['_id'], doc['commit'], str(doc['author_time'])]))
+            outf.write('\n')
 
-result = list(result)
-print(len(result))
-with open('data/2010-12-31.csv', 'w') as outf:
-    for doc in result:
-        outf.write(','.join([doc['_id'], doc['commit'], str(doc['author_time'])]))
-        outf.write('\n')
-
-result = client['openeuler']['commits'].aggregate([
-    {
-        '$project': {
-            '_id': 0, 
-            'repository': 1, 
-            'commit': 1, 
-            'author_time': 1
-        }
-    }, {
-        '$match': {
-            'author_time': {
-                '$lt': datetime(2021, 11, 1, 0, 0, 0, tzinfo=timezone.utc)
-            }
-        }
-    }, {
-        '$sort': {
-            'repository': 1, 
-            'author_time': 1
-        }
-    }, {
-        '$group': {
-            '_id': '$repository', 
-            'commit': {
-                '$last': '$commit'
-            }, 
-            'author_time': {
-                '$last': '$author_time'
-            }
-        }
-    }
-], allowDiskUse=True)
-
-result = list(result)
-print(len(result))
-with open('data/2021-10-31.csv', 'w') as outf:
-    for doc in result:
-        outf.write(','.join([doc['_id'], doc['commit'], str(doc['author_time'])]))
-        outf.write('\n')
+retrieve_latest_commits(year, month, day)

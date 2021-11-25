1ee5a5fdbcddb24d874f2c80543d3e7d27aeb674
Kai Gao <gaokaipku@qq.com>
2021-11-25 16:05:30 +0800

    suppress error mesaages, add 2019-9-30 statistics

--- cal_loc.sh
+++ cal_loc.sh
@@ -12,21 +12,10 @@ cut -d\, -f1,2 --output-delimiter=" " $wd/data/$year-$month-$day.csv | while rea
     cd $folder$repo
     git checkout --quiet -
     git checkout --quiet $commit
-    loc=`cloc --quiet . | grep '^SUM:' | tr -s ' ' | cut -d ' ' -f5`
+    loc=`cloc --quiet . | grep '^SUM:' | tr -s ' ' | cut -d ' ' -f5 2>/dev/null`
     git checkout --quiet -
     echo $repo,$commit,$loc
     echo $repo,$commit,$loc>>$wd/data/$year-$month-$day.loc
 done
 paste -d , $wd/data/$year-$month-$day.csv <(cut -d\, -f3 $wd/data/$year-$month-$day.loc) > $wd/data/$year-$month-$day
-
-# cut -d\, -f1,2 --output-delimiter=" " $wd/data/2021-10-31.csv | while read repo commit; do
-#     echo $repo $commit
-#     cd $folder$repo
-#     git checkout --quiet -
-#     git checkout --quiet $commit
-#     loc=`cloc --quiet . | grep '^SUM:' | tr -s ' ' | cut -d ' ' -f5`
-#     git checkout --quiet -
-#     echo $repo,$commit,$loc
-#     echo $repo,$commit,$loc>>$wd/data/2021-10-31.loc
-# done
-# paste -d , $wd/data/2021-10-31.csv <(cut -d\, -f3 $wd/data/2021-10-31.loc) > $wd/data/2021-10-31
+cut -d\, -f4 $wd/data/$year-$month-$day | awk '{a += $1} END {print a}'

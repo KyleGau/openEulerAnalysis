30f1d3436b45bd0b5961524bbbaf38d4dbf3bb87
Kai Gao <gaokaipku@qq.com>
2021-11-26 15:48:26 +0800

    fix script path

--- linux_delta.sh
+++ linux_delta.sh
@@ -1,4 +1,4 @@
 git clone git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git /data/repos/linux
 cd linux
 git log --all --numstat -M -C --full-history --pretty=tformat:"STARTOFTHECOMMIT:linux%n%H;%T;%P;%an;%ae;%at;%cn;%ce;%ct;%s%n%b%nNOTES%N" | gzip > /data/repos/linux.log.gz
-gunzip -c /data/repos/linux.log.gz | perl bin/extrgitPlog.perl | gzip > /data/repos/linux.delta.gz
+gunzip -c /data/repos/linux.log.gz | perl extrgitPlog.perl | gzip > /data/repos/linux.delta.gz
